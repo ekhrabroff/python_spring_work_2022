39795dd41a4243fd4c6c7c8b9b259cf4ec229bd3
ekhrabroff <e.khrabroff@gmail.com>
2022-04-27 15:19:41 +0300

    Домашнее задание 8

--- hometasks/task23.py
+++ hometasks/task23.py
@@ -1,3 +1,10 @@
+#todo: Взлом шифра
+#Вы знаете, что фраза зашифрована кодом цезаря с неизвестным сдвигом.
+#Попробуйте все возможные сдвиги и расшифруйте фразу.
+
+
+#grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin.
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def decode_char(letter, offset):
