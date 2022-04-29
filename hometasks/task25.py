d0fafa3577f15bb39c48fc03e830b434745e42d7
ekhrabroff <e.khrabroff@gmail.com>
2022-04-29 17:12:54 +0300

    Домашнее задание 8

---
+++ hometasks/task25.py
@@ -0,0 +1,33 @@
+#todo: Убрать повторяющиеся буквы и лишние символы
+#Построить по ключевой фразе часть алфавита. Взять все буквы по одному разу. Не буквы убрать.
+#Буквы должны идти в том порядке, в котором встретились во фразе в первый раз.
+
+#Input             	            Output
+#apple	                        aple
+#25.04.2022 Good morning !!	    godmrni
+
+s = input('Введите строку: ')
+out = ''
+
+s = s.lower()
+
+for i in s:
+    if i.isalpha():
+        if s.count(i) < 2:
+            out += i
+        else:
+            if i in out:
+                continue
+            else:
+                out += i
+    else:
+        continue
+            
+            
+print(out)
+
+             
+       
+            
+        
+
